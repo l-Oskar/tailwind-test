463de7d43dcab8bd20c91c0d1273c8e59368ce41
l-Oskar <fediuk_v@protonmail.com>
2025-05-23 15:53:21 +0300

    Ready project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { ThemeToggle } from "../components/ThemeTogle";
+import { useEffect } from "react";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-background">
+        {/* Theme Toggle */}
+        <ThemeToggle />
+        {/* Background Effects */}
+        {/* Navbar */}
+        {/* Main Content */}
+        {/* Footer */}
+      </div>
+    </>
+  );
+};
